d47cccc769cbb1a05bf849ee2de6cfe0839aa720
Alex Dao <azd2@duke.edu>
2017-03-31 23:26:56 -0400

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Google Home Actions for Logapps Challenge
+
+Written by Alex Dao
---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+process.env.DEBUG = 'actions-on-google:*';
+
+const ActionsSdkAssistant = require('actions-on-google').ActionsSdkAssistant;
+
+exports.sayNumber = (req, res) => {
+  const assistant = new ActionsSdkAssistant({request: req, response: res});
+
+  function mainIntent (assistant) {
+    console.log('mainIntent');
+    let inputPrompt = assistant.buildInputPrompt(true, '<speak>Hi! <break time="1"/> ' +
+          'I can read out an ordinal like ' +
+          '<say-as interpret-as="ordinal">123</say-as>. Say a number.</speak>',
+          ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
+    assistant.ask(inputPrompt);
+  }
+
+  function rawInput (assistant) {
+    console.log('rawInput');
+    if (assistant.getRawInput() === 'bye') {
+      assistant.tell('Goodbye!');
+    } else {
+      let inputPrompt = assistant.buildInputPrompt(true, '<speak>You said, <say-as interpret-as="ordinal">' +
+        assistant.getRawInput() + '</say-as></speak>',
+          ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
+      assistant.ask(inputPrompt);
+    }
+  }
+
+  let actionMap = new Map();
+  actionMap.set(assistant.StandardIntents.MAIN, mainIntent);
+  actionMap.set(assistant.StandardIntents.TEXT, rawInput);
+
+  assistant.handleRequest(actionMap);
+};
